5e133fb6be6094ff50ffc4cc076fb21a4709c56e
daniel morales <daniel_morales81@hotmail.commorales81@hotmail.com>
2024-06-30 16:49:42 -0400

    second commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$teams = array('team1', 'team2', 'team3');
+$count = 0;
+while ($count < count($teams)) {
+    echo "<li>$teams[$count] </li>";
+    $count++;
+}
+
+?>
+<h1><?php bloginfo('name') ?></h1>
+<p><?php bloginfo('description') ?></p>
+<ul>
+    <?php foreach ($teams as $team) : ?>
+        <li><?php echo $team; ?></li>
+    <?php endforeach; ?>
